31eaf7dbb0c92b1c8cfe2c9962d4ae10e2184cd5
Corwin96 <33621431+Corwin96@users.noreply.github.com>
2020-01-21 14:00:55 +0100

    Splitting into different contract files

--- ERC20/ERC20_Recipient.sol
+++ ERC20/ERC20_Recipient.sol
@@ -43,12 +43,4 @@ contract Recipient is Owner {
     }
 }
 
-contract Token_erc20 is ERC20, ERC20Detailed {
 
-    /**
-     * @dev Constructor that gives _msgSender() all of existing tokens.
-     */
-    constructor () public ERC20Detailed("TestTokenEagleWorks", "TTEW", 18) {
-        _mint(msg.sender, 10000 * (10 ** uint256(decimals())));
-    }
-}
