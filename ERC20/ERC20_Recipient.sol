0cb35cf9bd76511cf089ae3c25452fb804c64117
Corwin96 <33621431+Corwin96@users.noreply.github.com>
2020-01-21 16:43:55 +0100

    Update ERC20_Recipient.sol

--- ERC20/ERC20_Recipient.sol
+++ ERC20/ERC20_Recipient.sol
@@ -8,10 +8,14 @@ contract Recipient is Owner {
     
     ERC20 private _token;
     
+    uint32 public transactionCounter;
+    
     event addresChanged(address _newAddress);
+    event coinAdded(address _sender);
     
     constructor (address token) public { 
         _token = ERC20(token); 
+        transactionCounter = 0;
     }
     
     function setContractAddr(address _newTokenAddress) public onlyOwner {
@@ -31,5 +35,7 @@ contract Recipient is Owner {
     function addGameCoin() public {
        // token.approve(address(this), 0); THIS IS NEEDED BUT SHOULD BE IMPLEMENTED IN THE JAVASCRIPT. ALLOW THE CONTRACT TO USE YOUR TOKENS.
         _token.transferFrom(msg.sender, address(this), 1);
+        transactionCounter++;
+        emit coinAdded(msg.sender);
     }
 }
