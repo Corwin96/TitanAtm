632150b4a2e56b0f45c2b8ebaefd85a3f4e549e5
Corwin96 <33621431+Corwin96@users.noreply.github.com>
2020-01-21 15:43:02 +0100

    Update ERC20_Recipient.sol

--- ERC20/ERC20_Recipient.sol
+++ ERC20/ERC20_Recipient.sol
@@ -6,7 +6,9 @@ import "https://github.com/Corwin96/TitanAtm/blob/develop/ERC20/Owner.sol";
 
 contract Recipient is Owner {
     
-    ERC20 token = ERC20(0x883B6d3E97063858e72b82d030F7eA3B7556cb5A); //FILL IN WITH ERC20 ADDRESS
+    constructor (address token) public { 
+        ERC20 token = ERC20(address); 
+    }
     
 //    function setContractAddr(address addr) public onlyOwner {
 //        ERC20 = addr;
