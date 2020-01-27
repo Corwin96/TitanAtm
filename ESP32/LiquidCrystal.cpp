e8634d3002e0d0f6c4b33c1fd19e623128cf9116
CryingEagle1 <31490364+CryingEagle1@users.noreply.github.com>
2020-01-27 20:55:05 +0100

    Create LiquidCrystal.cpp

---
+++ ESP32/LiquidCrystal.cpp
@@ -0,0 +1,62 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+
+
+#define BACKLIGHT_PIN 13
+
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+//LiquidCrystal_I2C lcd(0x38, BACKLIGHT_PIN, POSITIVE);  // Set the LCD I2C address
+
+
+// Creat a set of new characters
+const uint8_t charBitmap[][8] = {
+   { 0xc, 0x12, 0x12, 0xc, 0, 0, 0, 0 },
+   { 0x6, 0x9, 0x9, 0x6, 0, 0, 0, 0 },
+   { 0x0, 0x6, 0x9, 0x9, 0x6, 0, 0, 0x0 },
+   { 0x0, 0xc, 0x12, 0x12, 0xc, 0, 0, 0x0 },
+   { 0x0, 0x0, 0xc, 0x12, 0x12, 0xc, 0, 0x0 },
+   { 0x0, 0x0, 0x6, 0x9, 0x9, 0x6, 0, 0x0 },
+   { 0x0, 0x0, 0x0, 0x6, 0x9, 0x9, 0x6, 0x0 },
+   { 0x0, 0x0, 0x0, 0xc, 0x12, 0x12, 0xc, 0x0 }
+   
+};
+
+void setup()
+{
+   int charBitmapSize = (sizeof(charBitmap ) / sizeof (charBitmap[0]));
+
+  // Switch on the backlight
+  pinMode ( BACKLIGHT_PIN, OUTPUT );
+  digitalWrite ( BACKLIGHT_PIN, HIGH );
+  
+  lcd.begin(16,2);               // initialize the lcd 
+
+   for ( int i = 0; i < charBitmapSize; i++ )
+   {
+      lcd.createChar ( i, (uint8_t *)charBitmap[i] );
+   }
+
+  lcd.home ();                   // go home
+  lcd.print(" Setup");  
+  lcd.setCursor ( 0, 1 );        // go to the next line
+  lcd.print ("  Starting   ");
+  delay(1000);
+}
+
+void loop()
+{
+   lcd.home ();
+   lcd.print("Send Titan token");
+   lcd.setCursor(0, 1);
+   lcd.print("Titan Arcade");
+   delay (2000);
+   lcd.clear();
+   lcd.home();
+   lcd.print("Look on the side");
+   lcd.setCursor(0, 1);
+   lcd.print("For more info");
+   delay(2000);
+   lcd.clear();
+}
